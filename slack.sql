6583209183fd6c74723832115e527faebfbfc165
Timothy Spann <tim.spann@snowflake.com>
2025-03-17 15:45:30 -0400

    Create slack.sql

---
+++ slack.sql
@@ -0,0 +1,15 @@
+  CREATE TABLE slackmessages (
+	messagerealname text NULL,
+	messagetype text NULL,
+	inputs text NULL,
+	messageid text NULL,
+	uuid text NOT NULL,
+	messageusername text NULL,
+	messagetimestamp text NULL,
+	messagepermalinkpublic text NULL,
+	filename text NULL,
+	messagechannel text NULL,
+	messagetext text NULL,
+	messageusertz text NULL,
+	messagesubtype text NULL
+);
