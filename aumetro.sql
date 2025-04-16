0f296c51cacc3af605bfe87439cc6c64651c7258
Timothy Spann <tim.spann@snowflake.com>
2025-04-16 13:36:02 -0400

    Update aumetro.sql

--- aumetro.sql
+++ aumetro.sql
@@ -1,11 +1,11 @@
-CREATE or REPLACE TABLE adelaideagency (
+CREATE or REPLACE TABLE ADELAIDEAGENCY (
 	agencyid text NULL,
 	agencyname text NULL,
 	agencyurl text NULL,
 	agencytimezone text NULL,
-    agency_lang text NULL,
-    agency_phone  text NULL,
-    agency_fare_url text NULL
+    agencylang text NULL,
+    agencyphone  text NULL,
+    agencyfareurl text NULL
 );
 
 describe table adelaideroutes;
@@ -25,9 +25,24 @@ CREATE OR REPLACE TABLE adelaidecalendar (
 
 CREATE OR REPLACE TABLE adelaidecalendar_dates (
 	serviceid text NULL,
-	"date" text NULL,
+	`date` text NULL,
 	exceptiontype text NULL
 );
+
+-- field named date is a problem
+-- ADELAIDERELEASE NOTES   <--- skip this table
+
+--- Routes putdatabaserecord.error
+-- Cannot convert CHOICE, type must be explicit ->>> needs a cast
+
+---- Trips 
+--- putdatabaserecord.error
+--- Cannot convert CHOICE, type must be explicit
+
+--- dont do text
+
+describe table ADELAIDEROUTES;
+
 
 CREATE OR REPLACE TABLE adelaidefeed_info (
 	feedpublishername text NULL,
@@ -38,8 +53,8 @@ CREATE OR REPLACE TABLE adelaidefeed_info (
 	feedversion text NULL
 );
 
-CREATE OR REPLACE TABLE adelaideroutes (
-	routeid text NULL,
+CREATE OR REPLACE TABLE ADELAIDEROUTES (
+	routeid TEXT NULL,
 	agencyid text NULL,
 	routeshortname text NULL,
 	routelongname text NULL,
@@ -48,9 +63,29 @@ CREATE OR REPLACE TABLE adelaideroutes (
 	routeurl text NULL,
 	routecolor text NULL,
 	routetextcolor text NULL,
-    RouteGroup text NULL
+    routegroup text NULL
 );
 
+select * from ADELAIDEROUTES;
+
+
+
+CREATE OR REPLACE TABLE ADELAIDETRIPS (
+	routeid text NULL,
+	serviceid text NULL,
+	tripid text NULL,
+	tripheadsign text NULL,
+	tripshortname text NULL,
+	directionid text NULL,
+	blockid text NULL,
+	shapeid text NULL,
+    wheelchairaccessible text NULL
+);
+
+select * from ADELAIDETRIPS;
+
+
+  
 CREATE OR REPLACE  TABLE adelaidestops (
 	stopid text NOT NULL,
 	stopcode text NULL,
@@ -86,17 +121,6 @@ CREATE OR REPLACE TABLE adelaidetransfers (
     mintransfertime text
 );
 
-CREATE OR REPLACE TABLE adelaidetrips (
-	routeid text NULL,
-	serviceid text NULL,
-	tripid text NULL,
-	tripheadsign text NULL,
-	tripshortname text NULL,
-	directionid text NULL,
-	blockid text NULL,
-	shapeid text NULL,
-    wheelchair_accessible text NULL
-);
 
 CREATE OR REPLACE TABLE adelaidefareattributes (
 	fareid text,
@@ -106,7 +130,7 @@ CREATE OR REPLACE TABLE adelaidefareattributes (
 	transfers text
 );
 
-CREATE OR REPLACE TABLE adelaidetripupdates (
+CREATE OR REPLACE TABLE ADELAIDETRIPUPDATES (
   stopsequence text NULL,
   arrivaltime text NULL,
   stopid text NULL,
@@ -115,14 +139,9 @@ CREATE OR REPLACE TABLE adelaidetripupdates (
   departuretime text NULL,
   triprouteid   text NULL,
     ts text NULL,
-     uuid text NULL
-  );
+     uuid text NULL);
 
-
-  describe table ADELAIDETRIPUPDATES;
-
-
-  CREATE OR REPLACE TABLE adelaideSERVICEALERTS (
+  CREATE OR REPLACE TABLE ADELAIDESERVICEALERTS (
 	ROUTEID4 STRING,
 	ROUTEID5 STRING,
 	DESCRIPTIONLANG STRING,
@@ -145,8 +164,6 @@ CREATE OR REPLACE TABLE adelaidetripupdates (
 	UUID STRING
   );
 
-
-  select * from adelaideSERVICEALERTS;
   
 CREATE OR REPLACE TABLE ADELAIDEVEHICLEPOSITIONS (
 	ROUTEID STRING,
@@ -163,3 +180,28 @@ CREATE OR REPLACE TABLE ADELAIDEVEHICLEPOSITIONS (
 	TIMESTAMP STRING,
 	TS STRING
     );
+
+
+
+    PutDatabaseRecord[id=31d642ae-161b-3034-6a57-8b3cd6062f81] Failed to put Records to database for FlowFile[filename=trips.txt]. Routing to failure.: org.apache.nifi.serialization.record.util.IllegalTypeConversionException: Cannot convert CHOICE, type must be explicit
+
+    Routes
+    putdatabaserecord.error
+Cannot convert CHOICE, type must be explicit
+
+
+  describe table ADELAIDETRIPUPDATES;
+
+  select * from adelaidetripupdates where departuretime is not null;
+
+  select* from ADELAIDEVEHICLEPOSITIONS where routeid is not null;
+
+  
+  select * from adelaideSERVICEALERTS where stopid is not null;
+  
+  describe table adelaidestop_times;
+
+  describe table ADELAIDEAGENCY;
+  
+  select * from ADELAIDECALENDAR_DATES
+  
